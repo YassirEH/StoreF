e7f15832293723652edda662a16a10cc5cb257e5
YassirEH <elhaloutiyassir@gmail.com>
2023-08-15 13:48:51 +0100

    b

--- Core/Application/Dto/ProductDto.cs
+++ Core/Application/Dto/ProductDto.cs
@@ -5,6 +5,5 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public double Price { get; set; }
-        public int Stock { get; set; }
     }
 }
