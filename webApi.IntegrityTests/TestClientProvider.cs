caa13429a3b9925b41f43b11296a51c9929c60b5
YassirEH <elhaloutiyassir@gmail.com>
2023-09-18 09:45:11 +0100

    semi working

--- webApi.IntegrityTests/TestClientProvider.cs
+++ webApi.IntegrityTests/TestClientProvider.cs
@@ -1,29 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
-using System.Net;
-using Xunit;
+﻿//using Microsoft.AspNetCore.Mvc.Testing;
+//using System.Net;
+//using Xunit;
 
-namespace webApi.IntegrityTests
-{
-    public class ProductControllerIntegrationTests : IClassFixture<WebApplicationFactory<Program>>
-    {
-        private readonly WebApplicationFactory<Program> _factory;
-        private readonly HttpClient _client;
+//namespace webApi.IntegrityTests
+//{
+//    public class ProductControllerIntegrationTests : IClassFixture<WebApplicationFactory<Program>>
+//    {
+//        private readonly WebApplicationFactory<Program> _factory;
+//        private readonly HttpClient _client;
 
-        public ProductControllerIntegrationTests(WebApplicationFactory<Program> factory)
-        {
-            _factory = factory;
-            _client = _factory.CreateClient();
-        }
+//        public ProductControllerIntegrationTests(WebApplicationFactory<Program> factory)
+//        {
+//            _factory = factory;
+//            _client = _factory.CreateClient();
+//        }
 
-        [Fact]
-        public async Task GetProducts_ReturnsOk()
-        {
-            // Arrange
-            var response = await _client.GetAsync("/api/Product/Get All");
+//        [Fact]
+//        public async Task GetProducts_ReturnsOk()
+//        {
+//            // Arrange
+//            var response = await _client.GetAsync("/api/Product/Get All");
 
-            // Assert
-            response.EnsureSuccessStatusCode();
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-        }
-    }
-}
+//            // Assert
+//            response.EnsureSuccessStatusCode();
+//            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+//        }
+//    }
+//}
